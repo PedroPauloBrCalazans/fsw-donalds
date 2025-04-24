c7d3959a7fb90fe1a603e53a2c45b7f6556da1d4
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-23 23:59:15 -0300

    feat: Componente da tela inicial (seja bem-vindo)

--- src/app/[slug]/page.tsx
+++ src/app/[slug]/page.tsx
@@ -1,4 +1,7 @@
 import { getRestaurantBySlug } from "@/data/get-restaurant-by-slug";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import ConsumptionMethodOption from "./components/consumption-method-option";
 
 interface RestaurantPageProps {
   params: Promise<{ slug: string }>;
@@ -7,7 +10,45 @@ interface RestaurantPageProps {
 const RestaurantPage = async ({ params }: RestaurantPageProps) => {
   const { slug } = await params;
   const restaurant = await getRestaurantBySlug(slug);
-  return <h1>{restaurant?.name}</h1>;
+  if (!restaurant) {
+    return notFound();
+  }
+
+  return (
+    <div className="h-screen flex flex-col items-center justify-center px-6 pt-24">
+      {/* LOGO E TITULO */}
+      <div className="flex flex-col items-center gap-2">
+        <Image
+          src={restaurant?.avatarImageUrl}
+          alt={restaurant?.name}
+          width={82}
+          height={82}
+        />
+        <h2 className="font-semibold">{restaurant.name}</h2>
+      </div>
+      {/*BEM VINDO */}
+      <div className="space-y-2 pt-24 text-center">
+        <h3 className="text-2xl font-semibold">Seja bem-vindo</h3>
+        <p className="opacity-55">
+          Escolha como prefere aproveitar sua refeição. Estamos oferecer
+          praticidade e sabor em cada detalhe!
+        </p>
+      </div>
+
+      <div className="pt-14 grid grid-cols-2 gap-4">
+        <ConsumptionMethodOption
+          buttonText="Para comer aqui"
+          imageAlt="Comer aqui"
+          imageUrl="/sandu.png"
+        />
+        <ConsumptionMethodOption
+          buttonText="Para levar"
+          imageAlt="Para levar"
+          imageUrl="/levar.png"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default RestaurantPage;
