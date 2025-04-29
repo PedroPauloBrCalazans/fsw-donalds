410086d8e9dc6f4a65235bfce99fc88433dd233d
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-04-28 23:58:14 -0300

    feat: Implementação inicial do ContextAPI + Implementação inicial do componente de carrinho + Ajustes no componente de detalhar produtos.

---
+++ src/app/[slug]/menu/components/cart-sheet.tsx
@@ -0,0 +1,31 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { useContext } from "react";
+import { CartContext } from "../contexts/cart";
+
+const CartSheet = () => {
+  const { isOpen, toggleCart, products } = useContext(CartContext);
+
+  return (
+    <Sheet open={isOpen} onOpenChange={toggleCart}>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Sacola</SheetTitle>
+          <SheetDescription></SheetDescription>
+        </SheetHeader>
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.name} - {product.quantity}
+          </h1>
+        ))}
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default CartSheet;
