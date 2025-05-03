c338b94409bc55ea2e34552d588e5ea4c02f23d6
Pedro Paulo Brauna Calazans <pedropaulo91.calazans@gmail.com>
2025-05-02 23:35:48 -0300

    feat: Implementação da funcionalidade para diminuir itens no carrinho.

--- src/app/[slug]/menu/components/cart-product-item.tsx
+++ src/app/[slug]/menu/components/cart-product-item.tsx
@@ -1,14 +1,17 @@
 import Image from "next/image";
-import { CartProduct } from "../contexts/cart";
+import { CartContext, CartProduct } from "../contexts/cart";
 import { formatCurrency } from "@/helpers/format-currency";
 import { Button } from "@/components/ui/button";
 import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
 
 interface CartItemProps {
   product: CartProduct;
 }
 
 const CartProductItem = ({ product }: CartItemProps) => {
+  const { decreaseProductQuantity } = useContext(CartContext);
+
   return (
     <div className="flex items-center justify-between">
       <div className="flex items-center gap-3">
@@ -25,7 +28,11 @@ const CartProductItem = ({ product }: CartItemProps) => {
           </p>
 
           <div className="flex items-center gap-1 text-center">
-            <Button className="w-7 h-7 rounded-lg" variant="outline">
+            <Button
+              className="w-7 h-7 rounded-lg"
+              variant="outline"
+              onClick={() => decreaseProductQuantity(product.id)}
+            >
               <ChevronLeftIcon />
             </Button>
             <p className="w-7 text-xs">{product.quantity}</p>
